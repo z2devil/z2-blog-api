9a8aa83b5ee71d97793f53cd50215b3eeff80528
linker <631471750@qq.com>
2022-05-20 11:52:38 +0800

    mod: allowed origin

--- src/main/java/com/z2devil/blog_api/config/GlobalCorsConfig.java
+++ src/main/java/com/z2devil/blog_api/config/GlobalCorsConfig.java
@@ -19,7 +19,7 @@ public class GlobalCorsConfig {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) *默认所有ip和端口都可以访问，也可以设置固定
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("https://z2devil.cn/*");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(false);
         //3) 允许的请求方式
